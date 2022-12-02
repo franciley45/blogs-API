488be52ac8c2517305ffb26b3009c32c0d5a9b34
root <testepa45@gmail.com>
2022-12-02 15:30:50 +0000

    requisito-7

---
+++ src/models/Category.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    name: DataTypes.STRING,
+  },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'categories',
+    })
+
+  return Category;
+}
--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
-	const User = sequelize.define('User', {
-		id: { type: DataTypes.INTEGER, primaryKey: true },
-		displayName: DataTypes.STRING,
-		email: DataTypes.STRING,
-		password: DataTypes.STRING,
-		image: DataTypes.STRING,
-	},
-		{
-			timestamps: false,
-			underscored: true,
-			tableName: 'users',
-		})
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'users',
+    })
 
-	return User;
+  return User;
 }
